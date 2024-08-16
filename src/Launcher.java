fd9121a43cab7619e94fc145f49751845b5085f7
alex garag <alex.garag@bk.ru>
2024-08-16 10:51:36 +0300

    сделал задел для истории операций

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,4 +1,5 @@
 import by.tms.lesson22.onl30.handlers.CalculatorHandler;
+import by.tms.lesson22.onl30.handlers.HistoryHandler;
 import by.tms.lesson22.onl30.handlers.TestHandler;
 import com.sun.net.httpserver.HttpServer;
 
@@ -10,6 +11,7 @@ public class Launcher {
         HttpServer localServer = HttpServer.create(new InetSocketAddress(8080), 0);
         localServer.createContext("/test", new TestHandler());
         localServer.createContext("/calculator", new CalculatorHandler());
+        localServer.createContext("/history", new HistoryHandler());
         localServer.start();
     }
 }
---
+++ src/by/tms/lesson22/onl30/handlers/HistoryHandler.java
@@ -0,0 +1,13 @@
+package by.tms.lesson22.onl30.handlers;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class HistoryHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
